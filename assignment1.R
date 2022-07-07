ae450ccf8d1b6e80370e7d29f747a5118ab07548
rrwuzz <108839913+rrwuzz@users.noreply.github.com>
2022-07-07 18:59:46 -0400

    Add files via upload

--- assignment1.R
+++ assignment1.R
@@ -1,22 +1,29 @@
-# R Assignment 1
+# R Assignment 1 Richard Wu
 
 
 # Create a variable 'x' that holds a value of 9
+x <- 9
 
 # Create a variable 'y' that holds a value of x
+y <- x
 
 # Create a variable 'z' that holds the sum of x and y
+z <- x + y
 
 # Clear the variable x from the workspace
+rm (x)
 
 # Clear all variables from the workspace
+rm (list = ls () )
 
 # Make a statement that directs to http://www.google.com
+browseURL ('http://www.google.com')
 
 # Show a list of all packages installed
+library ()
 
 # Show a list of packages currently installed and usable
-
+search ()
 
 
 # Magic Number Exercise
@@ -26,3 +33,4 @@
     # Subtract 6 from the answer.
     # Divide what you get by 3.
 # you should end up with the same number that you started with
+((9+2)*3-6)/3
